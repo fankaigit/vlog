9b26c6ddd84781084d98d27fa903f43561cfa42e
fankai <fankai@fklj.net>
2018-01-10 21:34:54 +0800

    verify username and password

--- src/components/User.vue
+++ src/components/User.vue
@@ -16,26 +16,41 @@
     <section id="login" v-else>
       <div class="field">
         <p class="control has-icons-left has-icons-right">
-          <input class="input" type="email" v-model="username" placeholder="用户名"/>
+          <input class="input" type="text" v-model="username" maxlength="20" placeholder="用户名"/>
           <span class="icon is-small is-left"><i class="fa fa-user"></i></span>
           <span class="icon is-small is-right" v-if="isValidUserName">
             <i class="fa fa-check"></i>
           </span>
         </p>
+        <div class="notice">
+           <span v-if="username !== '' && !isValidUserName">
+            用户名只能包含字母和数字，6-20个字符
+          </span>
+        </div>
       </div>
 
       <div class="field">
         <p class="control has-icons-left has-icons-right">
-          <input class="input" type="password" v-model="password" placeholder="密码">
+          <input class="input" type="password" v-model="password" maxlength="20" placeholder="密码">
           <span class="icon is-small is-left"><i class="fa fa-lock"></i></span>
           <span class="icon is-small is-right" v-if="isValidPassword">
             <i class="fa fa-check"></i>
           </span>
         </p>
+        <div class="notice">
+           <span v-if="password !== '' && !isValidPassword">
+            密码必须包含字母和数字，6-20个字符
+          </span>
+        </div>
       </div>
 
-      <div class="login button is-success" @click="login()">登录</div>
-      <div class="button is-warning is-small" @click="register()">注册</div>
+      <div class="field">
+        <div class="login button is-success" @click="login()">登录</div>
+        <div class="button is-warning is-small" @click="register()">注册</div>
+        <div class="notice">
+           <div style="text-align: center">{{actionNotice}}</div>
+        </div>
+      </div>
     </section>
 
   </div>
@@ -43,8 +58,8 @@
 
 <style scoped>
 
-  #login {
-    margin: 2em auto 2em;
+  #header {
+    margin-bottom: 2em;
   }
 
   .field {
@@ -54,12 +69,13 @@
 
   .button {
     display: block;
-    width: 90%;
     margin: auto auto 1em;
   }
 
-  .button.login {
-    margin-top: 2em;
+  .notice {
+    height: 1.5em;
+    overflow: hidden;
+    color: red;
   }
 
 </style>
@@ -72,11 +88,19 @@
     data: function () {
       return {
         username: '',
-        password: ''
+        password: '',
+        lastAction: null
       }
     },
     methods: {
+      validInput: function () {
+        return this.isValidUserName && this.isValidPassword
+      },
       register: function () {
+        if (!this.validInput()) {
+          return
+        }
+        this.lastAction = 'register'
         const data = {
           username: this.username,
           password: this.hashedPassword()
@@ -85,6 +109,10 @@
         this.$store.dispatch('register', data)
       },
       login: function () {
+        if (!this.validInput()) {
+          return
+        }
+        this.lastAction = 'login'
         const data = {
           username: this.username,
           password: this.hashedPassword()
@@ -93,6 +121,7 @@
         this.$store.dispatch('login', data)
       },
       logout: function () {
+        this.lastAction = 'logout'
         this.$store.dispatch('logout')
       },
       hashedPassword: function () {
@@ -106,18 +135,27 @@
     },
     computed: {
       isValidUserName: function () {
-        // TODO
-        return this.username !== ''
+        return this.username.match(/^[0-9a-zA-Z]{6,10}$/)
       },
       isValidPassword: function () {
-        // TODO
-        return this.password !== ''
+        return this.password.match(/[0-9]/) && this.password.match(/[a-zA-Z]/) && this.password.length >= 6
       },
       loggedIn: function () {
         return this.$store.getters.loggedIn
       },
       user: function () {
         return this.$store.state.user
+      },
+      actionNotice: function () {
+        if (!this.loggedIn) {
+          if (this.lastAction === 'login') {
+            return '登录失败'
+          } else if (this.lastAction === 'register') {
+            return '注册失败'
+          }
+        } else {
+          return ''
+        }
       }
     }
   }
