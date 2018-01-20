a6fd7a3622598c6b66c3de9ddaf78028599ae0c6
fankai <fankai@fklj.net>
2018-01-20 11:37:41 +0800

    add favicon

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vlog/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
